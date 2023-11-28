b8cdb34b1897cd3b410d1e5a35bdc4dde5a72389
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2023-11-28 19:56:44 +0100

    Can now kill the enemy (crazy stuff rigth)

--- Carbon_World/Assets/Enemy.cs
+++ Carbon_World/Assets/Enemy.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     Animator animator;
+    public float health = 1;
 
     public float Health {
         set {
             health = value;
 
-            if(health <= 0) {
+            if (health <= 0)
+            {
+                //When the enemy's health is 0 or less, call the Defeated() method and start the coroutine [https://docs.unity3d.com/ScriptReference/Coroutine.html] to remove the enemy
                 Defeated();
-                RemoveEnemy();
+                StartCoroutine(RemoveEnemyWithDelay());
             }
         }
         get {
@@ -20,9 +23,13 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    public float health = 1;
+    
 
-
+    private IEnumerator RemoveEnemyWithDelay() // This is a coroutine method that will wait for a certain amount of time before removing the enemy
+    {
+        yield return new WaitForSeconds(1.5f); // Adjust the delay time as needed 
+        RemoveEnemy(); 
+    }
     private void Start() {
         animator = GetComponent<Animator>();
     }
@@ -33,5 +40,6 @@ public class Enemy : MonoBehaviour
 
         public void RemoveEnemy() {
             Destroy(gameObject);
+            //Remove the enemy from the scene
         }
     }
