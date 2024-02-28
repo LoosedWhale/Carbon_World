60d0b2de4a838e4d41391bb73d2642f0b429d82f
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2024-02-28 22:19:52 +0100

    Boss is no longer useless (almost but not completely)
    after a feeew hours of suffering from my own bad code and lack of a will to live, the code for the boss now works with the players attacks and also connects to a healthbar to indicate that the player infact does something to the boss and not only stands there like an idiot

--- Carbon_World/Assets/Scripts/Boss.cs
+++ Carbon_World/Assets/Scripts/Boss.cs
@@ -1,18 +1,85 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Boss : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class Boss : MonoBehaviour {
+
+    public int health;
+    public int damage;
+    //private float timeBtwDamage = 1.5f;
+
+    public Slider healthBar;
+    private Animator anim;
+    public bool isDead;
+
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
+    }
+    public float Health
+    {
+        set
+        {
+            health = Mathf.FloorToInt(value);
+
+            if (health <= 0)
+            {
+                Defeated();
+                StartCoroutine(RemoveEnemyWithDelay());
+            }
+        }
+        get { return health; }
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void Defeated()
     {
-        
+        anim.SetTrigger("defend");
     }
+    
+
+    private IEnumerator RemoveEnemyWithDelay()
+    {
+        yield return new WaitForSeconds(1.5f);
+        RemoveEnemy();
+    }
+
+    public void RemoveEnemy()
+    {
+        Destroy(gameObject);
+    }
+    
+    private void Update()
+    {
+/*
+        if (health <= 25) {
+            anim.SetTrigger("stageTwo");
+        }
+
+        if (health <= 0) {
+            anim.SetTrigger("death");
+        }
+
+        // give the player some time to recover before taking more damage !
+        if (timeBtwDamage > 0) {
+            timeBtwDamage -= Time.deltaTime;
+        }
+*/
+        healthBar.value = health;
+    }
+    
+
+    /*
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // deal the player damage ! 
+        if (other.CompareTag("Player") && isDead == false) {
+            if (timeBtwDamage <= 0) {
+                other.GetComponent<Player>().health -= damage;
+            }
+        } 
+    }
+    */
 }
